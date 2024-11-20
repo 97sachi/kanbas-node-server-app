b9a604f025e99b86b576f7ed9deadc533835eee8
97sachi <156970519+97sachi@users.noreply.github.com>
2024-11-19 22:08:05 -0500

    22

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(cors({
     credentials: true,
-    origin: process.env.NETLIFY_URL || "http://localhost:3000",
+    origin: process.env.NETLIFY_URL || "https://a5--amazing-bunny-e6043a.netlify.app" || "http://localhost:3000",
   }
  ));
   
